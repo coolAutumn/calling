d127abd71f439bb48d583b39048efe1af5b83111
coolAutumn <autumn3376@gmail.com>
2017-01-02 17:21:03 +0800

    Calling protocol message's body

---
+++ calling-remoting/src/main/java/net/leeautumn/remoting/protocol/callingprotocol/CallingProtocolBody.java
@@ -0,0 +1,9 @@
+package net.leeautumn.remoting.protocol.callingprotocol;
+
+/**
+ * Created by LeeAutumn on 30/12/2016.
+ * blog: leeautumn.net
+ */
+public class CallingProtocolBody {
+    public Object body;
+}
