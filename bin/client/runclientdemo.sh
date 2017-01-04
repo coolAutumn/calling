091777aa7da28b83effba2f3809a45c73e8dc54f
coolAutumn <autumn3376@gmail.com>
2017-01-04 19:18:35 +0800

    new shell script

---
+++ bin/client/runclientdemo.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd ../lib
+sh ../bin/runserver.sh net.leeautumn.client.demo.ClientDemo
---
+++ runclientdemo.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd devenv/client/lib
+sh ../bin/runserver.sh net.leeautumn.client.demo.ClientDemo
